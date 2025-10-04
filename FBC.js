7409feea8553c243451bfa81581b35e3dfc86089
omprakashreddy123 <omprakashprakash47083@gmail.com>
2025-10-04 10:09:08 +0530

    Update FBC.js

--- FBC.js
+++ FBC.js
@@ -50,20 +50,8 @@ function Counter() {
   );
 }
 
-// function Counter() {
-//   const [count, setCount] = useState(0);
 
-//   const increment = () => {
-//     setCount(count + 1);
-//   };
-
-//   return (
-//     <div>
-//       <h2>Count: {count}</h2>
-//       <button onClick={increment}>Increment</button>
-//     </div>
-//   );
-// }
 
 export default Counter;
+
 
